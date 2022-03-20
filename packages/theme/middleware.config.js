fbc0015347697ecf43de2ca581167fc27c21f1cf
skel <skelstar@outlook.com>
2022-03-20 13:59:22 +0800

    set project name

--- packages/theme/middleware.config.js
+++ packages/theme/middleware.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   integrations: {
-    __replace_me__: {
-      location: '@vue-storefront/__replace_me__-api/server',
+    storefrontECommerce: {
+      location: '@vue-storefront/storefrontECommerce-api/server',
       configuration: {}
     }
   }
